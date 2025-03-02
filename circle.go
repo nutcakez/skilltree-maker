199873fbb47f891433c0f3b65b5407b4da2f16af
norbi <norbi@pop-os.localdomain>
2025-03-02 16:34:27 +0100

    doneish

--- circle.go
+++ circle.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"math"
+
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 )
@@ -13,12 +16,12 @@ type circle struct {
 
 func NewDefaultCircle(x, y, radius float32) *circle {
 	return &circle{
-		showBigRadius: true,
+		showBigRadius: false,
 		x:             x,
 		y:             y,
 		radius:        radius,
 		ownRadius:     10,
-		strokeWidth:   5,
+		strokeWidth:   2,
 		circles:       make([]*circle, 0),
 	}
 }
@@ -26,12 +29,45 @@ func NewDefaultCircle(x, y, radius float32) *circle {
 func (c *circle) Update() {}
 
 func (c *circle) Draw(screen *ebiten.Image) {
+	for _, v := range c.circles {
+		vector.StrokeLine(screen, c.x, c.y, v.x, v.y, 2, orange, true)
+	}
+
 	vector.DrawFilledCircle(screen, c.x, c.y, c.ownRadius, red, true)
+	if c.showBigRadius {
+		vector.StrokeCircle(screen, c.x, c.y, c.radius, c.strokeWidth, green, true)
+
+	}
 	for _, v := range c.circles {
 		v.Draw(screen)
 	}
 }
 
-func (c *circle) AddByRatio(a, b float32) {
+func (c *circle) AddByDegree(degree float64) *circle {
+	newX, newY := GetPointOnCircle(float64(c.x), float64(c.y), float64(c.radius), degree)
 
+	newCircle := NewDefaultCircle(float32(newX), float32(newY), 50)
+
+	c.circles = append(c.circles, newCircle)
+
+	return newCircle
+}
+
+func (c *circle) AddByRatio(a, b float32) *circle {
+	ratio := a / b
+	d := 360 * ratio
+
+	return c.AddByDegree(float64(d))
+}
+
+func GetPointOnCircle(cx, cy, radius, degree float64) (float64, float64) {
+	// Convert degrees to radians
+	radians := degree * (math.Pi / 180)
+
+	// Calculate new coordinates
+	newX := cx + radius*math.Cos(radians)
+	newY := cy + radius*math.Sin(radians)
+
+	fmt.Println(newX, newY)
+	return newX, newY
 }
--- main.go
+++ main.go
@@ -4,30 +4,71 @@ import (
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 var (
-	red = color.RGBA{255, 0, 0, 1}
+	red    = color.RGBA{255, 0, 0, 255}
+	green  = color.RGBA{0, 255, 0, 1}
+	orange = color.RGBA{255, 127, 80, 1}
 )
 
 func main() {
 	ebiten.SetWindowSize(1280, 960)
-	game := game{}
+	game := game{
+		objects: make([]IDrawUpdate, 0),
+	}
+
+	radiusShows := false
+
+	mainCircle := NewDefaultCircle(300, 300, 100)
+	mainCircle.showBigRadius = radiusShows
+	mainCircle.AddByRatio(1, 8)
+	mainCircle.AddByRatio(2, 8)
+	c1 := mainCircle.AddByRatio(3, 8)
+	c1.radius = 150
+	c1.showBigRadius = radiusShows
+	c2 := c1.AddByDegree(90)
+	c2.radius = 200
+	c2.AddByDegree(90)
+	c2.AddByDegree(130)
+	c2.AddByDegree(170)
+	c1.AddByDegree(180)
+	// c1.AddByDegree(220)
+	// c1.AddByDegree(240)
+	mainCircle.AddByRatio(4, 8)
+	mainCircle.AddByRatio(5, 8)
+	mainCircle.AddByRatio(6, 8)
+	mainCircle.AddByRatio(7, 8)
+	mainCircle.AddByRatio(8, 8)
+
+	game.objects = append(game.objects, mainCircle)
 
 	if err := ebiten.RunGame(&game); err != nil {
 	}
 }
 
-type game struct{}
+type game struct {
+	objects []IDrawUpdate
+}
 
 func (g *game) Update() error {
+
+	for i := range g.objects {
+		g.objects[i].Update()
+	}
 
 	return nil
 }
 
 func (g *game) Draw(screen *ebiten.Image) {
-	vector.StrokeCircle(screen, 10, 10, 10, 10, red, true)
+	for i := range g.objects {
+		g.objects[i].Draw(screen)
+	}
 }
 
 func (g *game) Layout(a, b int) (int, int) { return 1280, 960 }
+
+type IDrawUpdate interface {
+	Draw(*ebiten.Image)
+	Update()
+}
