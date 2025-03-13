6302b99297b7032a753789e79e03462ba764cadf
norbi <norbi@pop-os.localdomain>
2025-03-13 08:13:50 +0100

    cleanup, ready to embed into main project

--- main.go
+++ main.go
@@ -44,23 +44,26 @@ func main() {
 	otherMainCircle := NewDefaultImgNode(300, 700, images[2])
 	otherMainCircle.showBigRadius = radiusShows
 	otherMainCircle.startNode = true
-	c1 := otherMainCircle.AddImgByDegree(45, images[3])
-	c2 := c1.AddImgByDegree(45, images[4])
+	otherMainCircle.onActivate = func() {
+		fmt.Println("yes this is clicked")
+	}
+	c1 := otherMainCircle.AddByDegree(45, images[3])
+	c2 := c1.AddByDegree(45, images[4])
 
-	c2.AddImgByDegree(45, images[5])
-	c2.AddImgByDegree(90, images[7])
+	c2.AddByDegree(45, images[5])
+	c2.AddByDegree(90, images[7])
 	c2.showBigRadius = radiusShows
-	c3 := c1.AddImgByDegree(135, images[5])
-	c3.AddImgByDegree(45, images[6])
-	c4 := c3.AddImgByDegree(90, images[8])
+	c3 := c1.AddByDegree(135, images[5])
+	c3.AddByDegree(45, images[6])
+	c4 := c3.AddByDegree(90, images[8])
 
-	c5 := c4.AddImgByDegree(50, images[11])
-	c4.AddImgByDegree(100, images[12])
-	c4.AddImgByDegree(150, images[13])
+	c5 := c4.AddByDegree(50, images[11])
+	c4.AddByDegree(100, images[12])
+	c4.AddByDegree(150, images[13])
 
-	c5.AddImgByDegree(60, images[14])
-	c5.AddImgByDegree(90, images[15])
-	c5.AddImgByDegree(120, images[16])
+	c5.AddByDegree(60, images[14])
+	c5.AddByDegree(90, images[15])
+	c5.AddByDegree(120, images[16])
 
 	game.display.node = otherMainCircle
 	// game.objects = append(game.objects, otherMainCircle)
@@ -78,12 +81,6 @@ type game struct {
 }
 
 func (g *game) Update() error {
-	// g.panner.Update()
-	// for i := range g.objects {
-	// 	g.objects[i].Update(g.panner.offsetX, g.panner.offsetY, g.screen2OffsetX, g.screen2OffsetY, g.panner.zoom)
-	// }
-	//
-	// g.screen2.Clear()
 	g.display.Update()
 
 	return nil
@@ -92,26 +89,6 @@ func (g *game) Update() error {
 func (g *game) Draw(screen *ebiten.Image) {
 	msg := fmt.Sprintf(`FPS: %0.2f, TPS: %0.2f`, ebiten.ActualFPS(), ebiten.ActualTPS())
 	ebitenutil.DebugPrint(screen, msg)
-	// for i := range g.objects {
-	// 	g.objects[i].Draw(g.screen2)
-	// }
-	//
-	// // we want to show 500x500 picture
-	//
-	// op := ebiten.DrawImageOptions{}
-	// // if we zoom in it means we want to see a bigger picture from screen2 but scaled to 500x500
-	//
-	// // figure out the scale here
-	// op.GeoM.Scale(1/g.panner.zoom, 1/g.panner.zoom)
-	// op.GeoM.Translate(float64(g.screen2OffsetX), float64(g.screen2OffsetY))
-	//
-	// // which part of the stuff we want?
-	// rect := image.Rect(g.panner.offsetX, g.panner.offsetY, int(500*g.panner.zoom), int(500*g.panner.zoom))
-	// img := ebiten.NewImageFromImage(g.screen2.SubImage(rect))
-	//
-	// vector.StrokeRect(screen, 0+float32(g.screen2OffsetX), 0+float32(g.screen2OffsetY), 500, 500, 2, color.RGBA{255, 0, 0, 255}, true)
-	//
-	// screen.DrawImage(img, &op)
 	g.display.Draw(screen)
 }
 
--- node.go
+++ node.go
@@ -11,29 +11,14 @@ import (
 )
 
 type Node struct {
-	showBigRadius, active, startNode     bool
-	x, y, ownRadius, radius, strokeWidth float32
-	offsetX, offsetY                     int
-	childs                               []*Node
-	parents                              []*Node
-	img                                  *ebiten.Image
-	onClickEffect                        func()
-	requirement                          func() bool
-}
-
-func NewDefaultCircle(x, y, radius float32) *Node {
-	return &Node{
-		showBigRadius: false,
-		active:        false,
-		x:             x,
-		y:             y,
-		radius:        radius,
-		ownRadius:     10,
-		strokeWidth:   2,
-		childs:        make([]*Node, 0),
-		parents:       make([]*Node, 0),
-		img:           nil,
-	}
+	showBigRadius, active, startNode bool
+	x, y, radius, strokeWidth        float32
+	offsetX, offsetY                 int
+	childs                           []*Node
+	parents                          []*Node
+	img                              *ebiten.Image
+	onActivate                       func()
+	requirement                      func() bool
 }
 
 func NewDefaultImgNode(x, y float32, img *ebiten.Image) *Node {
@@ -43,7 +28,6 @@ func NewDefaultImgNode(x, y float32, img *ebiten.Image) *Node {
 		x:             x,
 		y:             y,
 		radius:        300,
-		ownRadius:     10,
 		strokeWidth:   2,
 		childs:        make([]*Node, 0),
 		img:           img,
@@ -63,22 +47,16 @@ func (c *Node) Update(offsetX, offsetY, windowOffsetX, windowOffsetY int, zoom f
 func (c *Node) checkClick(zoom float64, windowOffsetX, windowOffsetY int) bool {
 	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButton0) {
 		x, y := ebiten.CursorPosition()
-		if c.img != nil {
-			w := c.img.Bounds().Dx()
-			h := c.img.Bounds().Dy()
-			if PointInRect(x, y,
-				int(float64(c.x-float32(w/2)+float32(c.offsetX))/zoom+float64(windowOffsetX)),
-				int(float64(c.y-float32(h/2)+float32(c.offsetY))/zoom+float64(windowOffsetY)),
-				int(float64(w)/zoom),
-				int(float64(h)/zoom)) && c.CanBeActivated() {
-				c.active = !c.active
-				return true
-			}
-		} else {
-			if PointInCircle(c.x, c.y, c.ownRadius, int32(x), int32(y)) {
-				c.active = !c.active
-				return true
-			}
+		w := c.img.Bounds().Dx()
+		h := c.img.Bounds().Dy()
+		if PointInRect(x, y,
+			int(float64(c.x-float32(w/2)+float32(c.offsetX))/zoom+float64(windowOffsetX)),
+			int(float64(c.y-float32(h/2)+float32(c.offsetY))/zoom+float64(windowOffsetY)),
+			int(float64(w)/zoom),
+			int(float64(h)/zoom)) && c.CanBeActivated() {
+			c.active = !c.active
+			c.onActivate()
+			return true
 		}
 	}
 
@@ -99,23 +77,20 @@ func (c *Node) Draw(screen *ebiten.Image) {
 		color = red
 	}
 
-	if c.img == nil {
-		vector.DrawFilledCircle(screen, c.x, c.y, c.ownRadius, color, true)
-	} else {
-		op := ebiten.DrawImageOptions{}
-		op.GeoM.Translate(float64(c.x-float32(c.img.Bounds().Dx()/2)+float32(c.offsetX)), float64(c.y-float32(c.img.Bounds().Dy()/2)+float32(c.offsetY)))
-		screen.DrawImage(c.img, &op)
-		strokeWith := 5
-		vector.StrokeRect(screen,
-			float32(c.x-float32(c.img.Bounds().Dx()/2)+float32(c.offsetX)),
-			float32(c.y-float32(c.img.Bounds().Dy()/2)+float32(c.offsetY)),
-			float32(c.img.Bounds().Dx()),
-			float32(c.img.Bounds().Dy()),
-			float32(strokeWith),
-			color,
-			true,
-		)
-	}
+	op := ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(c.x-float32(c.img.Bounds().Dx()/2)+float32(c.offsetX)), float64(c.y-float32(c.img.Bounds().Dy()/2)+float32(c.offsetY)))
+	screen.DrawImage(c.img, &op)
+	strokeWith := 5
+	vector.StrokeRect(screen,
+		float32(c.x-float32(c.img.Bounds().Dx()/2)+float32(c.offsetX)),
+		float32(c.y-float32(c.img.Bounds().Dy()/2)+float32(c.offsetY)),
+		float32(c.img.Bounds().Dx()),
+		float32(c.img.Bounds().Dy()),
+		float32(strokeWith),
+		color,
+		true,
+	)
+
 	if c.showBigRadius {
 		vector.StrokeCircle(screen, c.x+float32(c.offsetX), c.y+float32(c.offsetY), c.radius, c.strokeWidth, green, true)
 	}
@@ -124,19 +99,7 @@ func (c *Node) Draw(screen *ebiten.Image) {
 	}
 }
 
-func (c *Node) AddByDegree(degree float64) *Node {
-	degree -= 90
-	newX, newY := GetPointOnCircle(float64(c.x), float64(c.y), float64(c.radius), degree)
-
-	newCircle := NewDefaultCircle(float32(newX), float32(newY), 50)
-	newCircle.parents = append(newCircle.parents, c)
-
-	c.childs = append(c.childs, newCircle)
-
-	return newCircle
-}
-
-func (c *Node) AddImgByDegree(degree float64, img *ebiten.Image) *Node {
+func (c *Node) AddByDegree(degree float64, img *ebiten.Image) *Node {
 	degree -= 90
 	newX, newY := GetPointOnCircle(float64(c.x), float64(c.y), float64(c.radius), degree)
 
@@ -148,14 +111,20 @@ func (c *Node) AddImgByDegree(degree float64, img *ebiten.Image) *Node {
 	return newNode
 }
 
-func (c *Node) AddByRatio(a, b float32) *Node {
+func (c *Node) AddByRatio(a, b float32, img *ebiten.Image) *Node {
 	ratio := a / b
 	d := 360 * ratio
 
-	return c.AddByDegree(float64(d))
+	return c.AddByDegree(float64(d), img)
 }
 
 func (c *Node) CanBeActivated() bool {
+	if c.active {
+		return false
+	}
+	if !c.requirement() {
+		return true
+	}
 	if c.startNode {
 		return true
 	}
